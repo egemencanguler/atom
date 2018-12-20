ba158a060bea37f501db42f09a7fa5b5eb1c6490
Egemen Can Güler <egemencanguler@gmail.com>
2018-12-20 14:44:49 +0300

    emine shader completed

---
+++ shader/Assets/Shader1/EmineCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EmineCamera : MonoBehaviour 
+{
+    public Material emineMaterial;
+
+    void Update()
+    {
+        Color color = GetComponent<Camera>().backgroundColor;
+        emineMaterial.SetColor("_BackgroundColor",color);
+    }
+}
